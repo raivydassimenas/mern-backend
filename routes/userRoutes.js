ffe1319cafa1568944a665574f7bfa7832bf6c47
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-11-22 18:25:33 +0200

    Create authMiddleware and protect user getMe router

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { registerUser } = require('../controllers/userController');
 const { reqisterUser, loginUser, getMe } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
 
 router.post('/', registerUser);
 router.post('/login', loginUser);
-router.get('/', getMe);
+router.get('/', protect, getMe);
 
 module.exports = router;
